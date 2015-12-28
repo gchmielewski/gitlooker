94f567779d06464cda22a48d7ce6e4f8974d57c3
gchmielewski <gchmielewski@post.pl>
2015-12-28 19:46:45 +0100

    Login2
    Others files

--- app/src/main/java/com/example/gitlooker/BaseFragment.java
+++ app/src/main/java/com/example/gitlooker/BaseFragment.java
@@ -1,11 +1,27 @@
 package com.example.gitlooker;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by grzesiek on 2015-12-28.
  */
 public class BaseFragment extends Fragment {
 
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
 
+    super.onCreate(savedInstanceState);
+
+    setHasOptionsMenu(true);
+  }
+
+  @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+    super.onCreateOptionsMenu(menu, inflater);
+
+
+  }
 }
--- app/src/main/java/com/example/gitlooker/MainActivity.java
+++ app/src/main/java/com/example/gitlooker/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
 
+
   public void onClick(View v) {
 
     final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
