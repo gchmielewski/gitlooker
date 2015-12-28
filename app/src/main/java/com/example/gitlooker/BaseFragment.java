6ab9a839d37114da57fbd548612fc610ce05d192
gchmielewski <gchmielewski@post.pl>
2015-12-28 11:39:07 +0100

    Login

---
+++ app/src/main/java/com/example/gitlooker/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.example.gitlooker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by grzesiek on 2015-12-28.
+ */
+public class BaseFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/example/gitlooker/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.gitlooker;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "ImageGridActivity";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+
+    //if (BuildConfig.DEBUG) {
+    //  Utils.enableStrictMode();
+    //}
+
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.main_activity);
+
+    //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+    //setSupportActionBar(myToolbar);
+
+    if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
+
+      final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+      ft.replace(R.id.fragment_container, new LoginFragment(), TAG);
+      ft.commit();
+    }
+
+  }
+
+
+  public void onClick(View v) {
+
+    final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    ft.replace(R.id.fragment_container, new RepositoryListFragment());
+    ft.commit();
+  }
+}
